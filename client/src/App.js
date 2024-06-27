7ab1185036c11fde5acfd28c3508ad3b63b4ad99
G0udaa <137202715+abdullahgouda@users.noreply.github.com>
2024-06-27 03:04:47 +0300

    Products_design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 
-import {React} from 'react';
+import {React, useState} from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Products from './components/products';
+import Data from "./Data.json"
 
 function App() {
+
+  const[products , setproducts] = useState(Data)
+  console.log(Data)
   return (
     <div className="App">
       <Header/>
+      <div className='products_section'>
+        <Products products = {products}/>
+        <div className='filter_side'> filter_side </div>
+      </div>
       <Footer/>
     </div>
   );
